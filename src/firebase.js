04785ba45f72a2548312fce198be2c72d40e09f1
mazen-alabasiiry <mazenabasiiry@gamil.com>
2022-04-12 19:52:53 +0200

    handel user functions

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,14 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, deleteUser } from 'firebase/auth'
 
 // Your web app's Firebase configuration
+
 const firebaseConfig = {
-    apiKey: "AIzaSyAFXKDW9vG1ByvuUWJUFSeWITe7JEBc-7c",
-    authDomain: "comfy-sloth-4ef35.firebaseapp.com",
-    projectId: "comfy-sloth-4ef35",
-    storageBucket: "comfy-sloth-4ef35.appspot.com",
-    messagingSenderId: "172102894458",
-    appId: "1:172102894458:web:c2cce74012de02f38f8cc3"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
 };
 
 // Initialize Firebase
